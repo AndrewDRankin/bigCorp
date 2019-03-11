395a13a0599df2609bff7028401ecef17d75ec86
Andy <wave@andrewdrankin.com>
2019-03-11 18:12:22 +0000

    Nav bar v1.0

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+((doc) => {
+    let login = doc.getElementById("loginButton"),
+    form = doc.getElementById("loginForm"),
+    toggle = () => {
+        form.style.display === "none" ?
+            form.style.display = "block" :
+             form.style.display = "none";
+    };
+    login.addEventListener("click", () => toggle())
+})(document);
